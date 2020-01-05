b83400d763676569777974cf42b90718a7b1e17e
Robert C. Alfredson <robert.c.alfredson@gmail.com>
2020-01-05 15:24:49 -0500

    Add music metadata endpoints (first draft)

--- index.js
+++ index.js
@@ -1,12 +1,79 @@
 const fs = require('fs');
 const stream = require('stream');
+const cors = require('cors');
+const glob = require('glob');
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const basePath = process.env.BASE_PATH;
 
 app.use(morgan('short'));
+
+const whitelist = ['https://dailypage.org', 'http://localhost:3000'];
+  const corsOptions = {
+    origin: (origin, callback) => {
+      if (whitelist.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  };
+  app.use(cors(corsOptions));
+  app.options('*', cors(corsOptions));
+
+app.get('/meta/music/artists', async (_, res) => {
+  res.send(fs.readdirSync(`${basePath}/meta/Artists`, { withFileTypes: true })
+      .filter(dirent => dirent.isDirectory())
+      .map(dirent => dirent.name))
+})
+
+app.get('/meta/music/albums', async (_, res) => {
+  res.send(glob.GlobSync(`${basePath}/meta/Artists/**/*`, {nodir: true}).matches.map(match => {
+    const dirName = path.dirname(Object.keys(match)[0]).split('/');
+
+    const artist = dirName[dirName.length - 1];
+    return {artist: artist, name: path.basename(Object.keys(match)[0])}
+  }));
+});
+
+app.get('/meta/music/songs', async (req, res) => {
+  let allTracks = [];
+  const albums = glob.GlobSync(`${basePath}/meta/Artists/**/*`, {nodir: true}).matches.map(match => {
+    const dirName = path.dirname(Object.keys(match)[0]).split('/');
+
+    const artist = dirName[dirName.length - 1];
+    return {artist: artist, name: path.basename(Object.keys(match)[0])}
+  });
+
+  albums.forEach((album) => {
+      const trackData = JSON.parse(fs.readFileSync(`${basePath}/meta/Artists/${album.artist}/${album.name}`).toString()).tracks
+      trackData.forEach((track) => {
+        allTracks.push({
+          id: track.id, title: track.name, album, artist: (track.artist ? track.artist : album.artist),
+        });
+      });
+  });
+  allTracks = allTracks.sort((a, b) => {
+    if (a.title > b.title) { return 1; }
+    if (b.title > a.title) { return -1; }
+    return 0;
+  });
+  res.send(allTracks);
+});
+
+app.get('/meta/music/artist/:artistID/albums', async (req, res) => {
+  res.send(glob.GlobSync(`${basePath}/meta/Artists/${req.params.artistID}/*`, {nodir: true}).matches.map(match => {
+    return path.basename(Object.keys(match)[0])
+  }));
+});
+
+app.get('/meta/music/artist/:artistID/:albumID', async (req, res) => {
+  const albumFile = JSON.parse(fs.readFileSync(`${basePath}/meta/Artists/${req.params.artistID}/${req.params.albumID}`).toString());
+  res.send(albumFile)
+});
 
 app.use('/meta', express.static(`${basePath}/meta`));
 
